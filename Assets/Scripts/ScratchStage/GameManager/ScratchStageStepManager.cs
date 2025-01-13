8c4248a6f20a63db24f20c443d4bd243f749cd91
NaiNai-Ja <Nattanon1257@gmail.com>
2025-01-13 13:56:18 +0700

    Create GameItems
    สร้างไอเทมสำหรัับด่านแผลถลอกทั้งหมด

--- Assets/Scripts/ScratchStage/GameManager/ScratchStageStepManager.cs
+++ Assets/Scripts/ScratchStage/GameManager/ScratchStageStepManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class ScratchStageStepManager : MonoBehaviour
 {
     public static ScratchStageStepManager instance;
-    Enum_StageStep thisScratchStage = Enum_StageStep.StepOne;
+    internal Enum_StageStep thisScratchStage = Enum_StageStep.StepOne;
 
     void Awake()
     {
