e7b7c2e1d309af53182ec781ded4bbef9a027a57
NaiNai-Ja <Nattanon1257@gmail.com>
2025-01-13 14:37:30 +0700

    Items can Dragable
    - ไอเทม สามารถลากได้แล้ว

--- Assets/Scripts/ScratchStage/UseableItems/Item_Alcohol.cs
+++ Assets/Scripts/ScratchStage/UseableItems/Item_Alcohol.cs
@@ -2,33 +2,6 @@ using UnityEngine;
 
 public class Item_Alcohol : MonoBehaviour, IUseable
 {
-    Vector2 originPosition;
-    DragableItem dragableItemScript;
-
-    public Vector2 OriginPosition { get => originPosition; set => value = originPosition; }
-
-    void Awake()
-    {
-        dragableItemScript = GetComponent<DragableItem>();
-        originPosition = transform.position;
-    }
-
-
-    void Update()
-    {
-        if (!dragableItemScript.isDragging)
-        {
-            ResetPosition();
-        }
-    }
-
-
-
-    public void ResetPosition()
-    {
-        transform.position = originPosition;
-    }
-
     public void UseItem()
     {
         throw new System.NotImplementedException();
