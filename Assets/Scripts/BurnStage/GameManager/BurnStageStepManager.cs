27165b112effd261def1a82a07a463b8f036ee35
651310297 <tonrak.5@hotmail.com>
2025-01-13 13:53:55 +0700

    Create item mockups

--- Assets/Scripts/BurnStage/GameManager/BurnStageStepManager.cs
+++ Assets/Scripts/BurnStage/GameManager/BurnStageStepManager.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BurnStageStepManager : MonoBehaviour {
     public static BurnStageStepManager instance;
-    Enum_BurnStageStep currentStep = Enum_BurnStageStep.StepOne;
+    internal Enum_BurnStageStep currentStep = Enum_BurnStageStep.StepOne;
 
     void Awake() {
         instance = this;
