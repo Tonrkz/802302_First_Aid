84b5c3952ad26c5ed049bc60a10c3ebb37f5fc33
651310297 <tonrak.5@hotmail.com>
2025-01-21 22:37:59 +0700

    Create CPRStageStepManager.cs

--- Assets/Scripts/CPRStage/UseableItems/Cellphone.cs
+++ Assets/Scripts/CPRStage/UseableItems/Cellphone.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 public class Cellphone : MonoBehaviour, IUseable {
     public void UseItem() {
         Debug.Log("Cellphone used!");
+        //Play Animation
+        //Play Sound
+        //Destroy Object
+        Destroy(gameObject);
     }
 }
