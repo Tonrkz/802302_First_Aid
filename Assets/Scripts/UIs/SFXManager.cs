08358697d8506631d8dfaf7d5526ef1685324104
651310297 <tonrak.5@hotmail.com>
2025-02-01 18:54:56 +0700

    Setup SFXObject prefab and SFXManager.cs

---
+++ Assets/Scripts/UIs/SFXManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour {
+    public static SFXManager instance;
+
+    [Header("References")]
+    [SerializeField] AudioSource SFXObject;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(this);
+        }
+    }
+
+    public void PlaySFXClip(AudioClip audioClip, Transform spawnTransform, float volume) {
+        AudioSource audioSource = Instantiate(SFXObject, spawnTransform.position, Quaternion.identity);
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.Play();
+        Destroy(audioSource.gameObject, audioClip.length);
+    }
+}
