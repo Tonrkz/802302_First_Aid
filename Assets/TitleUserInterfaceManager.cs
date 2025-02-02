393213c06c139c8607a1c8b4ebee2bc6d6fcaeda
651310297 <tonrak.5@hotmail.com>
2025-02-03 04:36:10 +0700

    Main menu UI Animation

--- Assets/TitleUserInterfaceManager.cs
+++ Assets/TitleUserInterfaceManager.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using DG.Tweening;
 
-public class TitleUserInterfaceManager : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class TitleUserInterfaceManager : MonoBehaviour {
+
+    [Header("References")]
+    [SerializeField] RectTransform titlePanel;
+    [SerializeField] RectTransform levelSelectionPanel;
+
+    [Header("Attributes")]
+    [SerializeField] int screenWidth;
+    [SerializeField] float tweenDuration = 1f;
+
+    void Start() {
+        screenWidth = Screen.width;
+        screenWidth = (int)(screenWidth * 1080f / Screen.height);
+
+        titlePanel.anchoredPosition = new Vector2(0, 0);
+        levelSelectionPanel.anchoredPosition = new Vector2(screenWidth, 0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnClickLevelSelectionButton() {
+        titlePanel.DOAnchorPos(new Vector2(-screenWidth * 1.5f, 0), tweenDuration);
+        levelSelectionPanel.DOAnchorPos(new Vector2(0, 0), tweenDuration);
+    }
+
+    public void OnClickMainMenuButton() {
+        titlePanel.DOAnchorPos(new Vector2(0, 0), tweenDuration);
+        levelSelectionPanel.DOAnchorPos(new Vector2(screenWidth, 0), tweenDuration);
     }
 }
